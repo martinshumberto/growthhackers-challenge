cc67ca9b601b6f1c05f156dd3638ea520b7ab3a2
Humberto Martins <humberto@consilio.com.br>
2022-05-23 10:37:03 -0300

    chore: bigbang initial project web strucutre

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>GrowthHackers</div>;
+}
+
+export default App;
---
+++ packages/web/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: parseInt(process.env.PORT) || 3000,
+    host: '0.0.0.0',
+  },
+  build: {
+    sourcemap: true,
+  },
+  plugins: [react(), tsconfigPaths()],
+});
