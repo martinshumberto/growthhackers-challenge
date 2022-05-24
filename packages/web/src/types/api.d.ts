2c6a423fddd472b3ed259b92d6a18ddb57f70c1f
Humberto Martins <humberto@consilio.com.br>
2022-05-24 14:52:39 -0300

    feat: type api in web client

---
+++ packages/web/src/types/api.d.ts
@@ -0,0 +1,38 @@
+export interface IProduct {
+  id?: string;
+  title: string;
+  description?: string | null;
+  category?: ICategory | null;
+  categoryId?: string | null;
+  status: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface ICategory {
+  id?: string;
+  title: string;
+  description?: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IPaginationMeta {
+  totalItems: number;
+  itemCount: number;
+  itemsPerPage: number;
+  totalPages: number;
+  currentPage: number;
+}
+
+export interface IPagination<Item = any> {
+  items: Item[];
+  meta: IPaginationMeta;
+}
+
+export interface IDefaultResponse {
+  message: string;
+  status: number;
+  timestamp?: string;
+  path?: string;
+}
